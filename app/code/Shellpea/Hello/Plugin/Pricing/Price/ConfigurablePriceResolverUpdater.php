68fee27af2ced331223c0d1f94f6b86f94fe9b73
Sofa-queen <sofa.zu@mail.ru>
2020-05-06 14:43:34 +0300

    новый файл:    app/code/Shellpea/Hello/Plugin/Pricing/Price/ConfigurablePriceResolverUpdater.php 	новый файл:    app/code/Shellpea/Hello/etc/di.xml

---
+++ app/code/Shellpea/Hello/Plugin/Pricing/Price/ConfigurablePriceResolverUpdater.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Shellpea\Hello\Plugin\Pricing\Price;
+
+class ConfigurablePriceResolverUpdater
+{
+    public function afterResolvePrice( $price )
+    {
+	    $price = 3456;
+            return $price;
+    }	    
+}
