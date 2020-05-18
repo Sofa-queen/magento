240f09485200b4f5bb3757117b6de662d65f005e
Sofa-queen <sofa.zu@mail.ru>
2020-05-18 13:46:44 +0300

    Record html to log

---
+++ app/code/Shellpea/HTML/Observer/RecordCodeHtml.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Shellpea\HTML\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+
+class RecordCodeHtml implements ObserverInterface
+{
+    protected $logger;
+
+    public function __construct(\Psr\Log\LoggerInterface $logger)    
+    {
+        $this->logger = $logger;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {      
+        $response = $observer->getEvent()->getData('response');
+        if (!$response) return;
+        $body = $response->getBody();
+        $this->logger->info($body);
+    }	    
+}
