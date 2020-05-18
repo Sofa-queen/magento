fc17990d6365dd7f4853da300c4aff997eabc696
Sofa-queen <sofa.zu@mail.ru>
2020-05-18 13:51:18 +0300

    ControllerRaw

---
+++ app/code/Shellpea/MyController/Controller/Page/View.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace Shellpea\MyController\Controller\Page;
+
+class View extends \Magento\Framework\App\Action\Action
+{
+    protected $resultRedirect;
+
+    /**
+     * @var \Magento\Framework\Controller\Result\RawFactory
+     */
+    protected $resultRawFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+       \Magento\Framework\Controller\Result\Redirect $resultRedirect
+    )
+    {
+       $this->resultRawFactory = $resultRawFactory;
+       $this->resultRedirect = $resultRedirect;
+       parent::__construct($context);
+    }
+
+    /**
+     * View  page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+       $result = $this->resultRawFactory->create();
+       $result->setContents('<strong>Hello World</strong>');
+
+       return $this->resultRedirect->setPath('/');
+       //return $result;
+    }
+}
+
