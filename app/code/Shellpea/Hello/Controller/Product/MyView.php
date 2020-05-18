e24a14228a48f5adcd37eb26fada1d8db395304d
Sofa-queen <sofa.zu@mail.ru>
2020-05-18 13:54:37 +0300

    return Raw

---
+++ app/code/Shellpea/Hello/Controller/Product/MyView.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Shellpea\Hello\Controller\Product;
+
+use Magento\Catalog\Controller\Product\View;
+use \Magento\Framework\Controller\Result\Raw;
+use \Magento\Framework\App\Action\Context;
+use \Magento\Catalog\Helper\Product\View as ViewHelper;
+use \Magento\Framework\Controller\Result\ForwardFactory;
+use \Magento\Framework\View\Result\PageFactory;
+
+class MyView extends View
+{
+    /**
+     * @var Raw
+     */
+    protected $resultRaw;
+
+    public function __construct(
+        Context $context,
+        Raw $resultRaw,
+        ViewHelper $viewHelper,
+        PageFactory $resultPageFactory,
+        ForwardFactory $resultForwardFactory
+
+    ) {
+        parent::__construct($context, $viewHelper, $resultForwardFactory, $resultPageFactory);
+        $this->resultRaw = $resultRaw;
+    }
+
+    public function execute ()
+    {
+        return $this->resultRaw->setContents('свершилось');
+    }
+}
