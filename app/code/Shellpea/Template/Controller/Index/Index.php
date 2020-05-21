8997e15d97f38882ad4ddc0eab00245d7d373f52
Sofa-queen <sofa.zu@mail.ru>
2020-05-21 11:40:03 +0300

    new template

--- app/code/Shellpea/Template/Controller/Index/Index.php
+++ app/code/Shellpea/Template/Controller/Index/Index.php
@@ -3,23 +3,30 @@ namespace Shellpea\Template\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-    protected $_resultPageFactory;
+    protected $resultPageFactory;
+
+    protected $resultRaw;
+
+    protected $layoutFactory;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\Controller\Result\Raw $resultRaw,
+        \Magento\Framework\View\LayoutFactory $layoutFactory
     ) {
-        $this->_resultPageFactory = $resultPageFactory;
+        $this->resultPageFactory = $resultPageFactory;
+        $this->resultRaw = $resultRaw;
+        $this->layoutFactory = $layoutFactory;
         return parent::__construct($context);
     }
 
     public function execute()
     {
-        return $this->_resultPageFactory->create();
-        $html = $resultPage->getLayout()
+        $result = $this->layoutFactory->create()
                 ->createBlock('Shellpea\Template\Block\Index')
                 ->setTemplate('Shellpea_Template::sayhello.phtml')
                 ->toHtml();
-        $this->getResponse()->setBody($html);
+        return $this->resultRaw->setContents($result);
     }
 }
