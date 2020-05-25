c05d7dba8c62ecc08724480febf9cb5c5d3a39f0
Sofa-queen <sofa.zu@mail.ru>
2020-05-25 19:38:36 +0300

    number of attributes

---
+++ app/code/Shellpea/Attributes/view/frontend/web/js/attributes.js
@@ -0,0 +1,11 @@
+define([
+        'jquery'
+    ],
+    function($){
+        return function (config, element) {
+            var attributes = $(".data.table.additional-attributes").find(".col.label");
+        console.log(attributes.length);
+        };
+    }
+);
+
