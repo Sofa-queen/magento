b24a5a4b6dce9d770a0ef255f68bc3e6485ce034
Sofa-queen <sofa.zu@mail.ru>
2020-06-03 17:15:56 +0300

    add attribute to product with data patch

---
+++ app/code/Shellpea/NewAttribute/Setup/Patch/Data/AddTextAttribute.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Shellpea\NewAttribute\Setup\Patch\Data;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddTextAttribute implements DataPatchInterface
+{
+    /** @var ModuleDataSetupInterface */
+    private $moduleDataSetup;
+
+    /** @var EavSetupFactory */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(\Magento\Catalog\Model\Product::ENTITY, 'custom_Attribute', [
+            'group' => 'General',
+            'type' => 'text',
+            'label' => 'Custom Attribute',
+            'input' => 'textarea',
+            'global' => \Magento\Catalog\Model\ResourceModel\Eav\Attribute::SCOPE_GLOBAL,
+            'used_in_product_listing' => true,
+            'user_defined' => false,
+            'visible_on_front' => true,
+            'visible' => true,
+            'required' => false,
+            'is_html_allowed_on_front' => true
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
