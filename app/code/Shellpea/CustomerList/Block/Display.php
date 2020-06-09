4c46abf298c9fdde10f63026b866fa37fc2f05f6
Sofa-queen <sofa.zu@mail.ru>
2020-06-09 13:09:33 +0300

    customer list

---
+++ app/code/Shellpea/CustomerList/Block/Display.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Shellpea\CustomerList\Block;
+
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\FilterGroupBuilder;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SortOrder;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+
+class Display extends Template
+{
+    public function __construct(
+        FilterBuilder $filterBuilder,
+        FilterGroupBuilder $filterGroupBuilder,
+        CustomerRepositoryInterface $customerRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        SortOrder $sortOrder,
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->filterBuilder = $filterBuilder;
+        $this->filterGroupBuilder = $filterGroupBuilder;
+        $this->customerRepository = $customerRepository;
+        $this->customerRepository = $customerRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->sortOrder = $sortOrder;
+    }
+
+    public function getCustomers()
+    {
+        $filterName = $this->filterBuilder
+            ->setField(CustomerInterface::FIRSTNAME)
+            ->setConditionType('eq')
+            ->setValue('zebra')
+            ->create();
+        $filterId = $this->filterBuilder
+            ->setField(CustomerInterface::EMAIL)
+            ->setConditionType('like')
+            ->setValue('%com')
+            ->create();
+        $filterGroup = $this->filterGroupBuilder
+              ->setFilters([$filterName, $filterId])
+              ->create();
+        $searchCriteria = $this->searchCriteriaBuilder
+            //->addFilter(['name', 'ghhgg', 'neq'],['entity_id',1, 'lteq'])
+            //->addFilter('entity_id',1, 'lteq')
+            ->setFilterGroups([$filterGroup])
+            ->create();
+
+        $customers = $this->customerRepository->getList($searchCriteria);
+
+        return $customers->getItems();
+    }
+}
---
+++ app/code/Shellpea/CustomerList/Controller/Index/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Shellpea\CustomerList\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_resultPageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->_resultPageFactory = $resultPageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->_resultPageFactory->create();
+    }
+}
