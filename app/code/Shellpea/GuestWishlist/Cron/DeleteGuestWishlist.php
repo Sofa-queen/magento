c3ea1eac9fd9f09e373fe7f003cc7d03a0405ddc
Sofa-queen <sofa.zu@mail.ru>
2020-08-06 20:46:24 +0300

    edited code

--- app/code/Shellpea/GuestWishlist/Cron/DeleteGuestWishlist.php
+++ app/code/Shellpea/GuestWishlist/Cron/DeleteGuestWishlist.php
@@ -1,20 +1,13 @@
 <?php
 namespace Shellpea\GuestWishlist\Cron;
 
-use Psr\Log\LoggerInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\Stdlib\DateTime\DateTime;
-use Magento\Wishlist\Model\Wishlist;
 
 class DeleteGuestWishlist
 {
-
-    protected $logger;
-
     protected $date;
-
-    private $wishlist;
 
     protected $scopeConfig;
 
@@ -23,14 +16,10 @@ class DeleteGuestWishlist
     public function __construct(
         ScopeConfigInterface $scopeConfig,
         ResourceConnection $connection,
-        LoggerInterface $logger,
-        Wishlist $wishlist,
         DateTime $date
     ) {
-        $this->scopeConfig = $scopeConfig;
         $this->connection = $connection->getConnection();
-        $this->wishlist = $wishlist;
-        $this->logger = $logger;
+        $this->scopeConfig = $scopeConfig;
         $this->date = $date;
     }
 
@@ -39,30 +28,7 @@ class DeleteGuestWishlist
         $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
         $time = $this->scopeConfig->getValue(self::XML_PATH_GUESTWISHLIST_TIME, $storeScope);
 
-        $select = $this->connection->select()->from(
-            ['wishlist']
-        )->where(
-            'customer_id = 0'
-        );
-        $data = $this->connection->fetchCol($select);
-        $removed = [];
-        foreach ($data as $value) {
-                $guest_wishlist = $this->wishlist->load($value);
-                $updated_at = $guest_wishlist->getUpdatedAt();
-
-                $date = $this->date->gmtDate();
-                $date1 = strtotime($date);
-                $date2 = strtotime($updated_at);
-                $diff = abs($date1 - $date2);
-                $years = floor($diff / (365*60*60*24));
-                $months = floor(($diff - $years * 365*60*60*24)/ (30*60*60*24));
-                $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
-                
-            if ($days >= $time) {
-                $removed[] = $value;
-                $guest_wishlist->delete();
-            }
-        }
-        $this->logger->log(100, print_r($removed, true));
+        $table = $this->connection->getTableName("wishlist");
+        $this->connection->delete($table, ["customer_id = 0", "datediff(now(), updated_at) >= $time"]);
     }
 }
--- app/code/Shellpea/GuestWishlist/Helper/GuestWishlist.php
+++ app/code/Shellpea/GuestWishlist/Helper/GuestWishlist.php
@@ -4,41 +4,15 @@ namespace Shellpea\GuestWishlist\Helper;
 
 class GuestWishlist
 {
-    protected $_wishlistRepository;
-
-    protected $_productRepository;
-
-    protected $_customerSession;
-
-    protected $wishlistFactory;
-
-    protected $wishlist;
-
-    public function __construct(
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Magento\Wishlist\Model\WishlistFactory $wishlistRepository,
-        \Magento\Wishlist\Model\WishlistFactory $wishlistFactory,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Wishlist\Model\Wishlist $wishlist
-    ) {
-        $this->_productRepository = $productRepository;
-        $this->_wishlistRepository= $wishlistRepository;
-        $this->wishlistFactory = $wishlistFactory;
-        $this->session = $customerSession;
-        $this->wishlist = $wishlist;
-    }
-
     public function addingProductsToTheCustomerWishlist($customerId)
     {
         $wishlistId = $this->session->getWishlistId();
-        //$wishlist = $this->wishlistFactory->create()->load($wishlistId);
-        //$wishlist->load($wishlistId);
-        $guestWishlist = $this->wishlist->load($wishlistId);
+        $guestWishlist = $this->wishlistFactory->create()->load($wishlistId);
         $wishlistCollection = $guestWishlist->getItemCollection();
         foreach ($wishlistCollection as $item) {
                  $productId = $item->getProduct()->getId();
                  $product = $this->_productRepository->getById($productId);
-                 $wishlist = $this->_wishlistRepository->create()->loadByCustomerId($customerId, true);
+                 $wishlist = $this->wishlistFactory->create()->loadByCustomerId($customerId, true);
                  $wishlist->addNewItem($product);
                  $wishlist->save();
         }
